dea95766113259b0a6a38c7e1d2c21f008453406
grandhikavyasree-art <grandhikavyasree00@gmail.com>
2025-12-22 16:14:04 +0530

    Update name in names array from 'kavs' to 'kavya'

--- Module1_Array/Task1.java
+++ Module1_Array/Task1.java
@@ -8,7 +8,7 @@ class Main {
             System.out.println(num[i]);
         }
         
-        String[] names = {"kavs","bhavs","madhu"};
+        String[] names = {"kavya","bhavs","madhu"};
         for(int i=0;i<names.length;i++){
             System.out.println(names[i]);
         }
